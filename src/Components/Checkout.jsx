bbffdc4b16098ffa4a06023ef30bda1a0eee9214
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-07 14:06:23 +0700

    using useForm

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -5,8 +5,13 @@ import { formattedPrice } from "../util/formatter";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import userProgressContext from "../store/UserPorgessContext";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 export default function Checkout() {
+  const form = useForm();
+  const { register, control, handleSubmit } = form;
+  //   const { name, ref, onChange, onBlur } = register("full-name"); // One approach
   const cartCtx = useContext(CartContext);
   const userPorgressCtx = useContext(userProgressContext);
 
@@ -19,22 +24,36 @@ export default function Checkout() {
     userPorgressCtx.hideCheckout();
   }
 
+  function onSubmit(data) {
+    console.log("form submitted", data);
+  }
+
   return (
     <Modal open={userPorgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h2>Checkout</h2>
         <p>Total Amount : {formattedPrice.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" name="full-name" />
-        <Input label="Email Address" type="email" id="email" name="email" />
-        <Input label="Street" type="text" id="street" name="street" />
+        <Input
+          label="Full Name"
+          type="text"
+          id="full-name"
+          {...register("full-name")}
+        />
+        <Input
+          label="Email Address"
+          type="email"
+          id="email"
+          {...register("email")}
+        />
+        <Input label="Street" type="text" id="street" {...register("street")} />
         <div className="control-row">
           <Input
             label="Postal Code"
             type="text"
             id="postal-code"
-            name="postal-code"
+            {...register("postal-code")}
           />
-          <Input label="City" type="text" id="city" name="city" />
+          <Input label="City" type="text" id="city" {...register("city")} />
         </div>
         <div className="modal-actions">
           <Button onClick={handleClose} type="button" textOnly>
@@ -43,6 +62,7 @@ export default function Checkout() {
           <Button>Submit Order</Button>
         </div>
       </form>
+      <DevTool control={control} />
     </Modal>
   );
 }
--- src/Components/UI/Input.jsx
+++ src/Components/UI/Input.jsx
@@ -1,8 +1,12 @@
-export default function Input({label, id, ...props}){
-    return (
-        <div className="control">
-            <label htmlFor={id}>{label}</label>
-            <input {...props} id={id} required/>
-        </div>
-    )
-}
+import { forwardRef } from "react";
+
+const Input = forwardRef(({ label, id, ...props }, ref) => {
+  return (
+    <div className="control">
+      <label htmlFor={id}>{label}</label>
+      <input {...props} id={id} ref={ref} />
+    </div>
+  );
+});
+
+export default Input;
