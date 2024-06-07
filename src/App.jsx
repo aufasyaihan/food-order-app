d2a85580659d015dfc35cad21e6fcb9bbec22163
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-07 10:57:53 +0700

    added UserProgressContextProvider Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./Components/Header";
 import Meals from "./Components/Meals";
 import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/UserPorgessContext";
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header />
-      <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
